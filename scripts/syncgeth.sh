53738cfc5c61f54336a1e5b7d99869498c0f1190
Connor <connor.aitken@gmail.com>
2018-06-11 18:44:32 -0700

    using file provisioner instead of sync_folder for scripts

--- scripts/syncgeth.sh
+++ scripts/syncgeth.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 echo "geth syncing underway"
+#screen -dmS geth \
 geth --testnet --syncmode "light" \
 --rpc --rpcapi db,eth,net,web3,personal,admin \
 --cache=1024  --rpcport 8545
